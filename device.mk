95dc78f2bf3fbc55b9a3ea8f1c8db055a58442f9
Sarthak Roy <sarthakroy2002@gmail.com>
2022-06-29 10:31:04 +0300

    merlin: Build android.hardware.boot@1.2

--- device.mk
+++ device.mk
@@ -22,3 +22,10 @@ LOCAL_PATH := device/xiaomi/merlin
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     android.hardware.fastboot@1.0-impl-mock.recovery
+
+# Additional binaries & libraries needed for recovery
+TARGET_RECOVERY_DEVICE_MODULES += \
+	android.hardware.boot@1.2
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+	$(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.boot@1.2.so
